bb2052c59b6a0676ad276319a7665e7837f97157
fletch8 <stampsfj04@gmail.com>
2021-02-25 14:29:42 -0500

    added prevent default after much trial and error, forgot to check READMe

--- src/MyList.js
+++ src/MyList.js
@@ -3,18 +3,92 @@ import './App.css'
 import ListItem from './ListItem'
 
 
+// class MyList extends Component {
+  
+//   state ={
+//     toDoItemArray: ['ice cream', 'dinosaurs', 'hobbits']
+//   }
+
+//   clearList (e) {
+//     this.setState({
+//       toDoItemArray: null
+//     })
+//   }
+
+//   render() {
+    
+//     let excitedPhrases = this.state.toDoItemArray.map( (phrase, index) => {
+//       return <ListItem key={index} doThis={phrase}/>
+//     })
+
+//     return (
+//       <div>
+//         <h1> Things I should stop procrastinating:</h1>
+//         <ul>
+//           { excitedPhrases }
+//         </ul>
+//         <button onClick={(e) => this.clearList}>Clear List</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default MyList
+
 class MyList extends Component {
+
+  constructor (props) {
+    super()
+    this.state = {
+      toDoItemArray: ['ice cream', 'dinosaurs', 'hobbits'],
+      newItem: ''
+    }
+  }
+
+  clearList (e) {
+    console.log("Clearing list!")
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
+  newItemChange(e){
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem(e){
+    e.preventDefault()
+    const newArray = this.state.toDoItemArray
+    newArray.push(this.state.newItem)
+        
+    this.setState({
+      toDoItemArray: newArray
+    })
+   
+  }
+
   render() {
-    let todoItems = todoItems.map((item, index) => (
-      <ListItem doThis={item} key={'todo' + index} />
+    let todoItems = this.state.toDoItemArray.map( (item, index) => (
+      <ListItem doThis={item} key={index} />
     ))
 
     return (
       <div>
-        <h1> Things I should stop procrastinating:</h1>
+        <h1>Things I should stop procrastinating:</h1>
         <ul>
-          { todoItems }
+          {todoItems}
         </ul>
+        <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
+        <form>
+          <input type="text"
+          placeholder="Type an item here"
+          onChange={(e) => this.newItemChange(e)}
+          value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
       </div>
     )
   }
